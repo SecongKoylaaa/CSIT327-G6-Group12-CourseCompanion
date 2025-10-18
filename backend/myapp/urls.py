b14d86dd54c0d4097e50e86e07772bd89c4d2d1d
SecongKoyla <marcbensec@gmail.com>
2025-10-18 20:31:14 +0800

    Fix frontend probelm - latest code

---
+++ backend/myapp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.root_redirect, name='root'),
+    path('register/', views.register_page, name='register'),
+    path('login/', views.login_page, name='login'),
+    path('home/', views.home_page, name='home'),
+    path('logout/', views.logout_page, name='logout'),
+    path('create-post-link/', views.create_post_link, name='create-post-link'),
+    path('create-post-image/', views.create_post_image, name='create-post-image'),
+    path('recover/', views.recover_password_page, name='recover-password'),
+    path('reset-password/<str:reset_token>/', views.reset_password_page, name='reset-password'),
+]
